d31d12bf45646cdea3c0c70044e8214658bdf0e7
Unknown <ksg304@nyu.edu>
2020-02-02 14:28:07 -0500

    vid fade for dont clear, audio

---
+++ Assets/Scripts/FadeVidTransparency.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class FadeVidTransparency : MonoBehaviour
+{
+    public VideoPlayer vidPlayer;
+    public bool fading;
+    public float fadeSpeed;
+    public float fadeTo = 0.01f;
+
+    void Awake()
+    {
+        vidPlayer = GetComponent<VideoPlayer>();
+    }
+
+    void Start()
+    {
+        SetFade();
+    }
+
+    void SetFade()
+    {
+        fading = true;
+    }
+
+    void Update()
+    {
+        if (fading)
+        {
+            vidPlayer.targetCameraAlpha = Mathf.Lerp(vidPlayer.targetCameraAlpha, fadeTo, Time.deltaTime * fadeSpeed);
+            //stop fading
+            if(Mathf.Abs(fadeTo - vidPlayer.targetCameraAlpha)  < 0.001f)
+            {
+                fading = false;
+            }
+        }
+    }
+}
