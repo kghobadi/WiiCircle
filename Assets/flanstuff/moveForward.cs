d274f66dadd3bd6f7f0ff3e1da9f9fd16781f827
flan <mfalacci@gmail.com>
2020-02-02 14:47:58 -0500

    fixed whip sound + other things

---
+++ Assets/flanstuff/moveForward.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveForward : MonoBehaviour
+{
+
+    public int speed;
+    public AudioSource whip; 
+    public float whipTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        whipTimer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, 0, 1) * Time.deltaTime * speed;
+
+        if (whipTimer > 0)
+        {
+            speed = 350;
+            whipTimer -= Time.deltaTime;
+        }
+        else if (whipTimer <= 0)
+        {
+            speed = 50; 
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            whipTimer += .5f;
+            whip.Play();
+        }
+         
+        
+    }
+}
