918d216ae5e624f04c035b8c839ede20459bd4cc
funsworkings <funsworkings@gmail.com>
2020-02-02 19:50:50 -0500

    added hover reset

---
+++ Assets/r/scripts/HoverReset.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.EventSystems;
+
+public class HoverReset : MonoBehaviour
+                         , IPointerEnterHandler
+{
+    [SerializeField] AudioClip clip;
+    [SerializeField] AudioSource src;
+
+    public void OnPointerEnter(PointerEventData dat){
+        src.PlayOneShot(clip);
+    }
+}
